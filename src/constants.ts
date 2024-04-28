aabcd9b1b0540b31bd0a3bfe0433300c894c34e1
Sagar Modhavadia - Network <modhavadia_sagar@network.lilly.com>
2024-04-28 13:26:08 -0400

    Updated Files

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-export const CONCLUSION_THEMES: any = {
-  neutral: "5DB1D1",
-  success: "90C978",
-  skipped: "DEDEDE",
-  cancelled: "D99294",
-  timed_out: "FF756D",
-  failure: "C23B23",
-  action_required: "FF3636",
-};
