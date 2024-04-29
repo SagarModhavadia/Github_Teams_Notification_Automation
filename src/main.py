21d1c0ecc71b588aa2ce493ea379b6a05e6488ca
Sagar Modhavadia - Network <modhavadia_sagar@network.lilly.com>
2024-04-29 11:06:27 -0400

    Updated Files

---
+++ src/main.py
@@ -0,0 +1,3 @@
+from webhook import send_sectioned_message
+
+send_sectioned_message()
---
+++ src/webhook.py
@@ -0,0 +1,100 @@
+import pymsteams
+import os
+import logging
+import json
+
+import requests.exceptions
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(threadName)s -  %(levelname)s - %(message)s')
+
+job_status = os.environ.get("STATUS")
+job_name = os.environ.get("JOB_NAME")
+workflow_name = os.environ.get("WORKFLOW_NAME")
+facts = os.environ.get("FACTS")
+repo_server_url = os.environ.get("REPO_SERVER_URL")
+repo_name = os.environ.get("REPO_NAME")
+hook_url = os.environ.get("WEBHOOK_URI")
+triggering_actor = os.environ.get("TRIGGERING_ACTOR")
+github_sha = os.environ.get("GITHUB_SHA")
+run_id = os.environ.get("RUN_ID")
+
+
+def send_sectioned_message():
+    """
+        This sends a message with sections.
+    """
+
+    input_data = determine_input()
+    # start the message
+    teams_message = pymsteams.connectorcard(hook_url)
+    teams_message.title(f"Workflow '{workflow_name}' {input_data['status']}")
+    teams_message.text(f"{repo_server_url}/{repo_name}/commit/{github_sha}")
+
+    # section 1
+    section_1 = pymsteams.cardsection()
+    # section_1.title(input_data["title"])
+    section_1.activityTitle(f"Triggering actor: {triggering_actor}")
+    section_1.activityImage(input_data["iconUrl"])
+
+    # add link button
+    teams_message.addLinkButton("Go to Action", f"{repo_server_url}/{repo_name}/actions/runs/{run_id}")
+
+    # add facts
+    for k, v in json.loads(facts).items():
+        section_1.addFact(k, v)
+    for k, v in dict({"Job name": f"{job_name}", "Workflow name": f"{workflow_name}"}).items():
+        section_1.addFact(k, v)
+    teams_message.addSection(section_1)
+    teams_message.color(input_data["color"])
+    # teams_message.printme()
+    # send
+    try:
+        teams_message.send()
+        evaluate_response(teams_message.last_http_response.status_code)
+    except requests.exceptions.Timeout as te:
+        logging.warning(te)
+        logging.warning("The Teams notification will be skipped due to the timeout exception!!!")
+
+
+def evaluate_response(resp_status_code):
+    if isinstance(resp_status_code, int) and \
+            0 <= resp_status_code <= 299:
+        logging.info("Response code ok: %s", resp_status_code)
+    else:
+        logging.error("Unexpected response: %s", resp_status_code)
+        raise ValueError(f"Unexpected response: '{resp_status_code}'")
+
+
+def determine_input():
+    """
+        This function determines and icon, color and a title text based on the status of the job.
+        The expected status values 'failed', 'cancelled', 'success'
+    """
+
+    msg = f"Job name: '{job_name}' of the workflow '{workflow_name}'"
+    match job_status:
+        case 'failure':
+            return {
+                "iconUrl": "",
+                "color": "ae1314",
+                "title": f"{msg} has failed",
+                "status": "failed"
+            }
+        case 'cancelled':
+            return {
+                "iconUrl": ""
+                           "cancelled.png",
+                "color": "ffec50",
+                "title": f"{msg} has been cancelled",
+                "status": "cancelled"
+            }
+        case 'success':
+            return {
+                "iconUrl": "",
+                "color": "2cc73b",
+                "title": f"{msg} has successfully finished",
+                "status": "succeeded"
+            }
+        case _:
+            logging.error("Unexpected response: %s", job_status)
+            raise ValueError(f"The job_status contains unexpected value: '{job_status}'")
