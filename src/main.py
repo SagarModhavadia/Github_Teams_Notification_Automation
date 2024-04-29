1d05d9cad9d7c41c407d216874363dd5cf48a369
Sagar Modhavadia - Network <modhavadia_sagar@network.lilly.com>
2024-04-29 14:38:57 -0400

    Updated Files

--- src/main.py
+++ src/main.py
@@ -26,7 +26,6 @@ def send_sectioned_message():
     teams_message_section.activityImage("https://cdn-icons-png.flaticon.com/512/2111/2111432.png")
     teams_message_section.activityText(f"by @[{triggering_actor}](https://github.com/{triggering_actor}) on ")
     # section 1
-    teams_message_section.addFact("Committed by:", )
     teams_message_section.addFact("Commit message:", f" ")
     teams_message_section.addFact("Files changed:", f" ")
     # add link button
