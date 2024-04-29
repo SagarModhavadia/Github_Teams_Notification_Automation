b0da1bd21148311b290db8eab61d0019a4f251a2
Sagar Modhavadia - Network <modhavadia_sagar@network.lilly.com>
2024-04-29 14:53:52 -0400

    Updated Files

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ run_id = os.environ.get("RUN_ID")
 def send_sectioned_message():
     # start the message
     msTeams = pymsteams.connectorcard(hook_url)
-    msTeams.summary("Changes committed by {triggering_actor}")
+    msTeams.summary(f"Changes committed by {triggering_actor}")
     teams_message_section = pymsteams.cardsection()
     
     teams_message_section.activityTitle(f"File changes committed on [{repo_name}]({repo_server_url}/{repo_name})")
