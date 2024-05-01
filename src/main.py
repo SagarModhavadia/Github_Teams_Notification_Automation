403af465b59957fe8c79db1f28d48113151b6dd4
Sagar Modhavadia - Network <modhavadia_sagar@network.lilly.com>
2024-05-01 17:40:32 -0400

    Updated Files

--- src/main.py
+++ src/main.py
@@ -84,7 +84,7 @@ def send_teams_bot_message(notificationURL):
             Column(items=[Image(url=icon_url, width="40px")], width="auto"),
             Column(
                 items=[
-                    TextBlock(text=f"CI #{run_number} | File changes committed on [{repo_name}]({repo_server_url}/{repo_name})", size=types.FontSize.MEDIUM)
+                    TextBlock(text=f"CI #{run_number} | File changes committed on [{repo_name}]({repo_server_url}/{repo_name})", size=types.FontSize.LARGE)
                 ],
                 width="stretch",
             ),
@@ -99,123 +99,27 @@ def send_teams_bot_message(notificationURL):
     containers.append(
         Container(
             items=[
-                TextBlock(
-                    text="**Some numbers for you**",
-                    size=types.FontSize.MEDIUM,
-                ),
                 ColumnSet(
                     columns=[
                         Column(
                             items=[
-                                TextBlock(text="_Total_"),
-                                TextBlock(text="_Done by you_"),
-                                TextBlock(text="_Done by other teams_"),
-                                TextBlock(text="_Still open_"),
-                                TextBlock(text="_Closed_"),
+                                TextBlock(text="Branch:"),
+                                TextBlock(text="Commit message:"),
+                                TextBlock(text="Files changed")
                             ]
                         ),
                         Column(
                             items=[
-                                TextBlock(text="5"),
-                                TextBlock(text="4"),
-                                TextBlock(text="3"),
-                                TextBlock(text="6"),
-                                TextBlock(text="1"),
-                            ],
-                            spacing=types.Spacing.MEDIUM,
+                                TextBlock(text=f"[{github_branch.upper()}]({repo_server_url}/{repo_name}/tree/{github_branch})"),
+                                TextBlock(text=f"{commit.commit.message}"),
+                                TextBlock(text=f"{modifiedFiles}")
+                            ]
                         ),
-                    ],
-                    separator=True,
-                ),
-            ],
-            spacing=types.Spacing.MEDIUM,
-        )
-    )
-
-    containers.append(
-        Container(
-            items=[
-                TextBlock(
-                    text="**Detailed Results**",
-                    size=types.FontSize.MEDIUM,
-                ),
-            ],
-            separator=True,
-            spacing=types.Spacing.EXTRA_LARGE,
-        )
-    )
-
-    sample_column_set: ColumnSet = ColumnSet(
-        columns=[
-            Column(items=[TextBlock(text="12312")]),
-            Column(items=[TextBlock(text="done", color=types.Colors.GOOD)]),
-            Column(items=[TextBlock(text="abc")]),
-            Column(
-                items=[
-                    Image(
-                        url="https://adaptivecards.io/content/down.png",
-                        width="20px",
-                        horizontal_alignment=types.HorizontalAlignment.RIGHT,
-                    )
-                ],
-                select_action=ActionToggleVisibility(
-                    title="More",
-                    target_elements=[
-                        TargetElement(
-                            element_id="toggle-me",
-                        )
-                    ],
-                ),
-            ),
-        ]
-    )
-
-    containers.append(
-        Container(
-            items=[
-                Container(
-                    items=[
-                        ColumnSet(
-                            columns=[
-                                Column(items=[TextBlock(text="**Number**")]),
-                                Column(items=[TextBlock(text="**Status**")]),
-                                Column(items=[TextBlock(text="**Topic**")]),
-                                Column(items=[TextBlock(text="")]),
-                            ],
-                            id="headline",
-                        ),
-                    ],
-                    style=types.ContainerStyle.EMPHASIS,
-                    bleed=True,
-                ),
-                Container(items=[sample_column_set]),
-                Container(
-                    items=[
-                        TextBlock(
-                            text="_Here you gonna find more information about the whole topic_",
-                            id="toggle-me",
-                            is_visible=False,
-                            is_subtle=True,
-                            wrap=True,
-                        )
                     ]
                 ),
-            ],
-        )
-    )
-    containers.append(
-        Container(
-            items=[
-                TextBlock(
-                    text=f"Icon used from: {icon_source}",
-                    size=types.FontSize.SMALL,
-                    horizontal_alignment=types.HorizontalAlignment.CENTER,
-                    is_subtle=True,
-                )
             ]
         )
     )
-
     card = AdaptiveCard.new().version("1.4").add_items(containers).create()
 
     sendMessage = requests.post(notificationURL, json = card.to_json())
