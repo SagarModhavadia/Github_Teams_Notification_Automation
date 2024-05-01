b645d1ae475519a66b510573b2d8f795f5f15d4b
Sagar Modhavadia - Network <modhavadia_sagar@network.lilly.com>
2024-05-01 16:56:06 -0400

    Updated Files

--- src/main.py
+++ src/main.py
@@ -218,7 +218,7 @@ def send_teams_bot_message(notificationURL):
         )
     )
 
-    card = AdaptiveCard.new().version("1.4").add_items(containers).create()
+    card = AdaptiveCard.new().version("1.5").add_items(containers).create()
 
     validator: SchemaValidator = SchemaValidator()
     result: Result = validator.validate(card)
