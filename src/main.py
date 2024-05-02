cac630a784af615bf2d84d5e4738143a727f5f4c
Sagar Modhavadia - Network <modhavadia_sagar@network.lilly.com>
2024-05-02 14:24:20 -0400

    Updated Files

--- src/main.py
+++ src/main.py
@@ -58,12 +58,9 @@ def evaluate_response(resp_status_code):
         raise ValueError(f"Unexpected response: '{resp_status_code}'")
 
 def replace_json_values(json, values):
-  # find all placeholders
-  placeholders = re.findall('$[\w ]+$', json)
-  assert len(placeholders) == len(values), "Please enter the values of all placeholders."
   # replaces all placeholders with values
   for k, v in values.items():
-      placeholder = "<%s>" % k
+      placeholder = "$%s$" % k
       json = json.replace(placeholder, v)
   return json
 
@@ -91,7 +88,7 @@ def send_teams_bot_message(notificationURL):
             'BTN_VIEW_STATUS': f'{repo_server_url}/{repo_name}/actions/runs/{run_id}',
             'BTN_VIEW_DIFFS': f'{repo_server_url}/{repo_name}/commit/{github_sha}'
         }
-        final_json = replace_json_values(str(json_payload),payload_mapping)
+        final_json = replace_json_values(json_payload,payload_mapping)
     sendMessage = requests.post(notificationURL, json = final_json)
 
 if teams_channel_webhook_url:
