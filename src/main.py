c1d479e3ecadf13d7ea3b3ff9e534f605b856521
Sagar Modhavadia - Network <modhavadia_sagar@network.lilly.com>
2024-05-03 11:34:40 -0400

    Updated Files

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ def send_teams_channel_message(notificationURL):
     
     teams_message_section.activityTitle(f"CI #{run_number} | File changes committed on [{repo_name}]({repo_server_url}/{repo_name})")
     teams_message_section.activityImage("https://cdn-icons-png.flaticon.com/512/2111/2111432.png")
-    teams_message_section.activityText(f"by [@{commit.committer.login}](https://github.com/{commit.committer.login}) on {commit.last_modified}")
+    teams_message_section.activityText(f"by [@{commit.committer.login}](https://github.com/{commit.committer.login}) on {commit.commit.committer.date}")
     # section 1
     teams_message_section.addFact("Branch:", f"[{github_branch.upper()}]({repo_server_url}/{repo_name}/tree/{github_branch})")
     teams_message_section.addFact("Commit message:", f"{commit.commit.message}")
@@ -58,10 +58,6 @@ def evaluate_response(resp_status_code):
         logging.error("Unexpected response: %s", resp_status_code)
         raise ValueError(f"Unexpected response: '{resp_status_code}'")
 
-def datetime_from_utc_to_local(utc_datetime):
-    tzLocal = pytz.timezone('America/New_York') 
-    return pytz.utc.localize(utc_datetime).astimezone(tzLocal)
-
 def replace_placeholder_values(source, values):
   # replaces all placeholders with values
   for k, v in values.items():
@@ -86,7 +82,7 @@ def send_teams_bot_message(notificationURL):
             'IMAGE':f'https://cdn-icons-png.flaticon.com/512/2111/2111432.png',
             'IMAGE_ALT':f'{commit.committer.login}', 
             'MESSAGE_HEADER': f'File changes committed on [{repo_name}]({repo_server_url}/{repo_name})',
-            'MESSAGE_SUB_HEADER': f'by [{commit.committer.login}](https://github.com/{commit.committer.login}) on {datetime_from_utc_to_local(commit.commit.committer.date)}',
+            'MESSAGE_SUB_HEADER': f'by [{commit.committer.login}](https://github.com/{commit.committer.login}) on {commit.commit.committer.date}',
             'BRANCH': f'[{github_branch.upper()}]({repo_server_url}/{repo_name}/tree/{github_branch})',
             'COMMIT_MESSAGE': f'{commit.commit.message}',
             'FILES_CHANGED': f'{modifiedFiles}',
